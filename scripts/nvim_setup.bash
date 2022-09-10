f0b5a32faa1ad61e9fa18182f14b1845d30d0c4c
Zephan M. Enciso <zmenciso@pm.me>
2022-09-10 01:14:22 -0400

    Replace nvim setup script with bash

--- scripts/nvim_setup.bash
+++ scripts/nvim_setup.bash
@@ -1,4 +1,4 @@
-#!/usr/local/bin/fish
+#!/bin/bash
 
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
