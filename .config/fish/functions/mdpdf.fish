9e3984e4c90b778dc6dbde93017ce4d7ff929897
Zephan M. Enciso <zmenciso@pm.me>
2023-03-03 10:21:32 -0500

    mdpdf: Do not require output filename

--- .config/fish/functions/mdpdf.fish
+++ .config/fish/functions/mdpdf.fish
@@ -9,17 +9,18 @@ function mdpdf -d "Convert markdown to PDF via pandoc and xelatex"
 
 	set pandoc_args "--pdf-engine=xelatex" \
 	"--highlight-style=tango" \
-	"-V geometry:\"top=2.54cm, bottom=2.54cm, left=2.54cm, right=2.54cm\"" \
+	# "-V geometry:\"top=2.54cm, bottom=2.54cm, left=2.54cm, right=2.54cm\"" \
 	"-V colorlinks -V urlcolor=NavyBlue" \
 	"-H /tmp/head.tex"
 
-	argparse --min-args=2 'h/help' 'n/number-sections' 'f/font=?' -- $argv
+	argparse --min-args=1 'h/help' 'n/number-sections' 'f/font=?' 'o/output=?' -- $argv
 
 	if test -n "$_flag_h"
 		echo 'mdpdf [OPTIONS] INPUT OUTPUT'
 		echo '  -h  --help		Show this message'
 		echo '  -n  --number	Number sections and produce a ToC'
 		echo '  -f  --font		Change main font'
+		echo '  -o  --ouptut	Specify output filename'
 
 		exit 0
 	end
@@ -37,5 +38,10 @@ function mdpdf -d "Convert markdown to PDF via pandoc and xelatex"
 		set pandoc_args --toc -N $pandoc_args
 	end
 
-	eval pandoc $pandoc_args $argv[1] -o $argv[2]
+	set output $_flag_o
+	if test -z "$output"
+		set output (string split '.' $argv[1])[1].pdf
+	end
+
+	eval pandoc $pandoc_args $argv[1] -o $output
 end
