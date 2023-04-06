08af9b3703e0ca8620c3d23f9a4db41549877b09
Zephan M. Enciso <zmenciso@pm.me>
2023-04-06 12:55:20 -0400

    Neovim: Fix alternate tab glyph spacing

--- .config/nvim/lua/setup.lua
+++ .config/nvim/lua/setup.lua
@@ -382,7 +382,7 @@ require('lualine').setup{
 				modified_status = true,
 				symbols = {
 					modified = ' ',      -- Text to show when the buffer is modified
-					alternate_file = ' ', -- Text to show to identify the alternate file
+					alternate_file = '  ', -- Text to show to identify the alternate file
 					directory =  '',     -- Text to show when the buffer is a directory
 				},
 			}
