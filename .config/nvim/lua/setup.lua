f1af7fbc6b7d8963b00c0f5c3c34885af623a7ad
Zephan M. Enciso <zmenciso@pm.me>
2023-04-06 10:41:53 -0400

    Neovim: Change buffer symbols

--- .config/nvim/lua/setup.lua
+++ .config/nvim/lua/setup.lua
@@ -380,6 +380,11 @@ require('lualine').setup{
 			{
 				'buffers',
 				modified_status = true,
+				symbols = {
+					modified = ' ',      -- Text to show when the buffer is modified
+					alternate_file = ' ', -- Text to show to identify the alternate file
+					directory =  '',     -- Text to show when the buffer is a directory
+				},
 			}
 		},
 		lualine_b = {},
