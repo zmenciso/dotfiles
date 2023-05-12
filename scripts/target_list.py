8543a1bc7b51971762d3b88d24a01fcbc09b78ba
Zephan M. Enciso <zmenciso@pm.me>
2023-05-12 10:03:19 -0400

    Waybar: Simplify files

--- scripts/target_list.py
+++ scripts/target_list.py
@@ -32,11 +32,7 @@ CATEGORIES = {
     },
     'WAYBAR': {
         '.config/waybar/config.jsonc',
-        '.config/waybar/style.css',
-        '.config/waybar/config_top.jsonc',
-        '.config/waybar/style_top.css',
-        '.config/waybar/config_right.jsonc',
-        '.config/waybar/style_right.css'
+        '.config/waybar/style.css'
     },
     'FISH': {
         '.config/fish/config.fish',
