97cc775b5a6e510dd0f04ac9791e0665cb324b4a
Zephan M. Enciso <zmenciso@pm.me>
2023-07-10 10:47:16 -0400

    marp-all: Do all .md files if file not specified

--- .config/fish/functions/marp-all.fish
+++ .config/fish/functions/marp-all.fish
@@ -1,4 +1,8 @@
 function marp-all
+    if test (count $argv) = '0'
+        set argv '*.md'
+    end
+
 	marp --pdf --pdf-outline --pdf-notes --allow-local-files $argv
 	marp --notes $argv
 	marp $argv
