2df9ee46921290e1318bb83d9c9d5edb09fbf57b
Zephan M. Enciso <zmenciso@pm.me>
2024-01-18 11:10:06 -0500

    Fish: `marp-setup` now customizes the date

--- .config/fish/functions/marp-setup.fish
+++ .config/fish/functions/marp-setup.fish
@@ -1,6 +1,6 @@
 function marp-setup
     if not set -q MARP_TEMPLATE
-        set MARP_TEMPLATE ~/doc/COAIHWL/Marp/COAIHWL_Marp_1
+        set MARP_TEMPLATE ~/vault/doc/COAIHWL/Marp/COAIHWL_Marp_1
     end
 
     if test (count $argv) = 0
@@ -13,14 +13,21 @@ function marp-setup
     echo 'Copying Markdown template...'
     if not test -e $argv
         mkdir -p $argv
-    else if not test -d $argv
-        echo "ERROR: `$argv` is not a valid destination"
+    end
+    if not test -d $argv
+        echo "ERROR: $argv is not a valid destination"
         return 2
     end
-
     cp $MARP_TEMPLATE/*.md $argv
     mv $argv/*.md $argv/(basename $argv).md
 
     echo 'Copying resources...'
     cp $MARP_TEMPLATE/*.svg $argv
+
+    echo 'Customizing template...'
+    set date (basename $argv | grep -E --only-matching "[0-9]+-[0-9]+-[0-9]+")
+    if test -z "$date"
+        set date (date +%Y-%m-%d)
+    end
+    sed -i "s/yyyy-mm-dd/$date/g" $argv/*.md
 end
