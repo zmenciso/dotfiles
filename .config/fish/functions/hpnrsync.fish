058a7d24d127a2d79060186b7eba58f71b94c4c3
Zephan M. Enciso <zmenciso@pm.me>
2024-07-10 15:08:58 -0400

    hpnrsync: Use human-readable progress

--- .config/fish/functions/hpnrsync.fish
+++ .config/fish/functions/hpnrsync.fish
@@ -1,4 +1,4 @@
 function hpnrsync --wraps=rsync\ -e\ \'hpnssh\ -p\ 2222\'\ -rauL\ --info=progress2 --description alias\ hpnrsync\ rsync\ -e\ \'hpnssh\ -p\ 2222\'\ -rauL\ --info=progress2
-  rsync -e 'hpnssh -p 2222' -rauL --info=progress2 $argv
+  rsync -e 'hpnssh -p 2222' -rauhL --info=progress2 $argv
         
 end
