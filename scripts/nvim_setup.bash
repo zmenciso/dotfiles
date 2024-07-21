166abf7fdc3f1b01106bb5b3c122e1fdb8fb14e9
Zephan Enciso <zmenciso@pm.me>
2024-07-21 15:51:19 -0400

    Reorginize scripts

--- scripts/nvim_setup.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- scripts/tmux_setup.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/fish
-
-mkdir -p ~/.config/tmux-powerline
-git clone https://github.com/erikw/tmux-powerline.git ~/.config/tmux-powerline
