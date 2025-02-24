18f5b40be6c2279fe58dee1bad3c268a81e8242c
Zephan M. Enciso <zmenciso@pm.me>
2025-02-24 14:43:34 -0500

    Setup: Use regular instead of black carbonfox theme

--- scripts/setup/nightfox_setup.fish
+++ scripts/setup/nightfox_setup.fish
@@ -7,4 +7,4 @@ yay -S sassc gtk-engine-murrine
 git clone git@github.com:Fausto-Korpsvart/Nightfox-GTK-Theme.git
 cd Nightfox-GTK-Theme/themes
 
-./install.sh -l -s compact --tweaks carbon black
+./install.sh -l -s compact --tweaks carbon
