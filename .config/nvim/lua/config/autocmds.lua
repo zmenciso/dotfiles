22b448a76a6f884c869a5dbc5347154a0108024f
Zephan Enciso <zmenciso@pm.me>
2025-06-09 09:30:59 -0400

    LazyVim: Redefine text wrapping autocommand

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "tex", "latex", "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt.textwidth = 80
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+    vim.opt.breakindent = true
+    vim.opt.wrapscan = false
+    vim.opt.linebreak = true
+  end,
+})
