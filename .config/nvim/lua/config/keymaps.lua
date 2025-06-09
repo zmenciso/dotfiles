cfdebd9403884a82ce69ab9cdbcc7f480f13bb5e
Zephan Enciso <zmenciso@pm.me>
2025-06-09 09:31:53 -0400

    LazyVim: Add keymaps for word deletion and splits

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,10 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = LazyVim.safe_keymap_set
+local map = vim.keymap.set
 
+local function lmap(mode, shortcut, command, opts)
+  vim.api.nvim_set_keymap(mode, shortcut, command, opts)
+end
+
+-- Terminal
 map("n", "<c-t>", function()
   Snacks.terminal()
 end, { desc = "Terminal (cwd)" })
 
-vim.api.nvim_set_keymap("n", "<Space>fx", "<cmd>Xplr<cr>", { noremap = true })
+-- File manager
+lmap("n", "<leader>fx", "<cmd>Xplr<cr>", { noremap = true })
+
+-- Map Ctrl-Backspace to delete the previous word in insert mode.Add
+lmap("!", "<C-BS>", "<C-w>", { noremap = true })
+lmap("!", "<C-h>", "<C-w>", { noremap = true })
+
+-- Splits
+lmap("n", "<c-v>", "<cmd>vsplit<cr>", {})
+lmap("n", "<c-x>", "<cmd>split<cr>", {})
