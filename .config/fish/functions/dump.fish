f452c49ba4412ec18529708fb7c7f74c7e58ccb8
Zephan Enciso <zmenciso@pm.me>
2025-06-09 13:40:05 -0400

    Fish: Dump uses a truncated hash for file name by default

--- .config/fish/functions/dump.fish
+++ .config/fish/functions/dump.fish
@@ -3,17 +3,23 @@ function dump
     set -l DUMPDIR /tank/www/dump
     set -l URL https://dump.duck-pond.org
 
-    argparse --min-args=1 h/help r/rename -- $argv
+    set -l HASH 4 # Number of characters to keep from hash
+
+    argparse --min-args=1 k/keep h/help r/rename -- $argv
     or return
 
     if set -ql _flag_h
-        echo "Usage: dump [-h | --help] [-r | --rename] URLS"
+        echo 'dump [OPTIONS] FILES'
+        echo '  -h  --help        Show this message'
+        echo '  -k  --keep        Keep the original filename'
+        echo '  -r  --rename      Manually specify the filename'
         return
     end
 
     set urls
 
     for file in $argv
+        # Bypass copy if file does not exist
         if not test -f $file
             echo -n 'File not found: '
             set_color red
@@ -23,28 +29,47 @@ function dump
             continue
         end
 
-        if set -ql _flag_r
+        if set -ql _flag_k
+            # Keep the original filename
+            set dest (basename $file)
+        else if set -ql _flag_r
+            # Manually rename file
             read -p "echo -n 'Rename '; set_color green; echo -n $file; set_color normal; echo -n ' > '" dest
         else
-            set dest (basename $file)
+            # Replace the filename with a truncated hash
+            set -l hash (md5sum $file | cut -f 1 -d ' ')
+            set -l ext (path extension $file)
+            set dest (string sub -l $HASH $hash)$ext
         end
 
         if test (cat /etc/hostname) = $REMOTE
+            # Already on $REMOTE, so just copy the file
             cp $file $DUMPDIR/$dest
         else
+            # Send the file with hpnscp (requires ssh config)
             hpnscp $file $REMOTE:$DUMPDIR/$dest
         end
 
+        # Add new URL to list
         set urls $urls $URL/$dest
     end
 
     if test -n "$urls"
         echo $urls
-        echo $urls | wl-copy
+
+        # Automatically copy URL
+        switch $XDG_SESSION_TYPE
+            case wayland
+                echo $urls | wl-copy
+            case x11
+                echo $urls | xclip
+        end
+
     else
         set_color red
         echo 'Error: No files uploaded'
         set_color normal
+
     end
 
 end
