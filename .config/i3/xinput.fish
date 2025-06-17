18e26ef382a5c5d7d7eb5d02cbaa632f7db629f3
Zephan M. Enciso <zmenciso@pm.me>
2025-06-17 14:56:18 -0400

    i3: xinput script now does the smoothest brain scanning for devices

--- .config/i3/xinput.fish
+++ .config/i3/xinput.fish
@@ -1,8 +1,22 @@
 #!/usr/bin/env fish
 
-# Adjust this file to your needs
-#   - `xinput list` 
-#   - `xinput list-props` 
-#   - `xinput set-prop`
+set -l DEVICES 'Mizar Mouse' 'Mizar Mouse'
+set -l TYPES pointer pointer
+set -l PROPS 'Accel Speed' 'Scroll Method Enabled'
+set -l VALUES -1 '0 0 0'
 
-xinput set-prop 10 298 -1
+for i in (seq (count $DEVICES))
+  set -l dev $DEVICES[$i]
+  set -l type $TYPES[$i]
+  set -l prop $PROPS[$i]
+  set -l val $VALUES[$i]
+
+  set -l id (xinput | grep $dev | grep $type)
+  set -l id (string match -r 'id=([0-9]+)' $id[1])[2]
+
+  set -l num (xinput list-props $id | grep $prop)
+  set -l num (string match -r '\(([0-9]+)\)' $num[1])[2]
+
+  echo xinput set-prop $id $num $val
+  xinput set-prop $id $num $val
+end
