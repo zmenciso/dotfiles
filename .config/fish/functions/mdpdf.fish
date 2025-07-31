ca71118eac58a3eefabc46ddd043ea5bc5880b98
Zephan Enciso <zmenciso@pm.me>
2025-07-31 09:33:09 -0400

    mdpdf: Switch to eisvogel template

--- .config/fish/functions/mdpdf.fish
+++ .config/fish/functions/mdpdf.fish
@@ -1,63 +1,85 @@
 function mdpdf -d "Convert markdown to PDF via pandoc and xelatex"
-    set texhead '%Change background color for inline code and quote style
-\definecolor{bgcolor}{HTML}{FCE8E8}
-\let\oldtexttt\texttt
+    # Uses the excellent template from 
+    # https://github.com/Wandmalfarbe/pandoc-latex-template
 
-\renewcommand{\texttt}[1]{
-  \colorbox{bgcolor}{\oldtexttt{#1}}
-}
+    # ---
+    # title: "The Document Title"
+    # author: [Example Author, Another Author]
+    # date: "2017-02-20"
+    # keywords: [Markdown, Example]
+    # ...
 
-\usepackage{siunitx}
-\usepackage{hyperref}
+    # See the pandoc variables here: https://pandoc.org/MANUAL.html#variables-for-latex
 
-\@ifpackageloaded{hyperref}{}{%
-  \RequirePackage{float}%
-  \RequirePackage[unicode, pdftex, CJKbookmarks]{hyperref}%
-  \RequirePackage{algorithm}%
-}
+    # Custom Variables:
+    #   titlepage (defaults to false)
+    #   titlepage-color
+    #   titlepage-text-color (defaults to 5F5F5F)
+    #   titlepage-rule-color (defaults to 435488)
+    #   titlepage-rule-height (defaults to 4)
+    #   titlepage-logo
+    #   titlepage-background
+    #   page-background
+    #   page-background-opacity (defaults to 0.2)
+    #   caption-justification (defaults to raggedright)
+    #   toc-own-page (defaults to false)
+    #   listings-disable-line-numbers (defaults to false)
+    #   listings-no-page-break (defaults to false)
+    #   disable-header-and-footer (default to false)
+    #   header-left (defaults to the title)
+    #   header-center
+    #   header-right (defaults to the date)
+    #   footer-left (defaults to the author)
+    #   footer-center
+    #   footer-right (defaults to the page number)
+    #   footnotes-pretty (defaults to false)
+    #   footnotes-disable-backlinks (defaults to false)
+    #   book (defaults to false)
+    #   logo-width (defaults to 35mm), see https://github.com/tweh/tex-units
+    #   first-chapter (defaults to 1)
+    #   float-placement-figure (defaults to H)
+    #   table-use-row-colors (defaults to false)
+    #   code-block-font-size (defaults to \small)
+    #   watermark (defaults to none)
 
-\urlstyle{rm}
-\hypersetup{
-  breaklinks=true,
-  unicode=true,
-  citebordercolor=0.7 1 0.7,
-  filebordercolor=0 0.5 0.5,
-  linkbordercolor=1 0.7 0.7,
-  menubordercolor=1 0.7 0.0,
-  urlbordercolor=0.8 0.8 1,
-  runbordercolor=1 0.7 1
-}'
+    set -l USERDIR (pandoc --version | grep 'User data directory')
+    set -l USERDIR (string split ': ' $USERDIR)[2]
 
-    set pandoc_args "--pdf-engine=xelatex" \
-        "--highlight-style=tango" \
-        "-V colorlinks -V urlcolor=NavyBlue" \
-        "-V geometry:\"top=2.54cm, bottom=2.54cm, left=2.54cm, right=2.54cm\"" \
-        "-H head.tex"
+    if not test -f $USERDIR/templates/eisvogel.latex
+        set TEMPDIR (mktemp -d)
+        git clone https://github.com/Wandmalfarbe/pandoc-latex-template.git $TEMPDIR/eisvogel
+        git clone https://github.com/jgm/pandoc-templates.git $TEMPDIR/base
 
-    argparse --min-args=1 h/help n/number-sections 'f/font=?' 'o/output=?' -- $argv
+        if not test -d $USERDIR/templates
+            mkdir -p $USERDIR/templates
+        end
+
+        /bin/cp $TEMPDIR/base/* $USERDIR/templates
+        /bin/cp $TEMPDIR/eisvogel/template-multi-file/* $USERDIR/templates
+    end
+
+    set pandoc_args --listings \
+        "--template=eisvogel" \
+        "--pdf-engine=pdflatex"
+    # "--highlight-style=tango" \
+
+    argparse --min-args=1 h/help n/number 'o/output=?' -- $argv
 
     if set -ql _flag_h
         echo 'mdpdf [OPTIONS] INPUT'
-        echo '  -h  --help        Show this message'
-        echo '  -n  --number      Number sections and produce a ToC'
-        echo '  -f  --font        Change main font'
-        echo '  -o  --ouptut      Specify output filename'
+        echo '  -h  --help          Show this message'
+        echo '  -o  --ouptut        Specify output filename'
+        echo '  -n  --number        Number sections'
+        echo '  -t  --toc           Create table of content'
 
         exit 0
     end
 
-    set rpath (dirname $argv[1])
-
-    if not test -f $rpath/head.tex
-        echo -n $texhead >$rpath/head.tex
-        set cleanup
-    end
-
-    set -ql _flag_f
-    and set pandoc_args --mainfont=$_flag_f $pandoc_args
-
     set -ql $_flag_n
-    and set pandoc_args --toc -N $pandoc_args
+    and set pandoc_args --number-sections $pandoc_args
+
+    set -ql $_flag_t
+    and set pandoc_args --toc $pandoc_args
 
     if set -ql _flag_o
         set output $_flag_o
@@ -67,7 +89,4 @@ function mdpdf -d "Convert markdown to PDF via pandoc and xelatex"
 
     echo pandoc $pandoc_args $argv[1] -o $output
     pandoc $pandoc_args $argv[1] -o $output
-
-    set -ql cleanup
-    and /bin/rm $rpath/head.tex
 end
