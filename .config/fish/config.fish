e163a374e1e0e845f3bdeb39b46ddd3e525e6e45
Zephan M. Enciso <zmenciso@pm.me>
2025-11-26 12:46:12 -0500

    Fish: Keep bar cursor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ set -Ux LC_ALL 'en_US.UTF-8'
 set -Ux EDITOR helix
 
 # Cursor options
-set fish_vi_force_cursor
+# set fish_vi_force_cursor
 
 # set fish_cursor_default block
 # set fish_cursor_insert line
@@ -16,6 +16,9 @@ set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/lib /usr/local/lib64
 
 # Colored GCC warnings and errors
 set -x GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# If not Vim, then set cursor to line for insert
+__fish_cursor_xterm line
 
 # Vim key bindings
 # fish_vi_key_bindings
